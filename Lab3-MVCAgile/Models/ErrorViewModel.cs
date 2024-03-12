ba06e6b22f8cff4f19ae2ed388615c271c6b98bb
pnguy118 <phucnhat2111@gmail.com>
2024-03-12 18:15:54 -0400

    Add project files.

---
+++ Lab3-MVCAgile/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab3_MVCAgile.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
