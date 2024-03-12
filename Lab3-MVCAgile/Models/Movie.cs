af06fdb9203523de78d4ea7466e700eefbf50397
pnguy118 <phucnhat2111@gmail.com>
2024-03-12 18:41:40 -0400

    add search function

--- Lab3-MVCAgile/Models/Movie.cs
+++ Lab3-MVCAgile/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Lab3_MVCAgile.Models
 {
@@ -6,9 +7,12 @@ namespace Lab3_MVCAgile.Models
     {
         public int Id { get; set; }
         public string? Title { get; set; }
+
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
